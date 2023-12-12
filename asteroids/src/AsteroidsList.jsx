593119cb1ef544555b6e10aa1994c6f71ce65066
Ksusha <ksuksuksuha@gmail.com>
2023-12-12 17:11:30 +0100

    + date picker for asteroids

--- asteroids/src/AsteroidsList.jsx
+++ asteroids/src/AsteroidsList.jsx
@@ -3,6 +3,13 @@ import { useEffect, useState } from 'react';
 import axios from "axios";
 import { AsteroidCard } from './AsteroidCard.jsx';
 import { Link } from 'react-router-dom';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { registerLocale } from  "react-datepicker";
+import { enGB } from 'date-fns/locale';
+
+
+registerLocale('en-GB', enGB)
 
 
 function AsteroidsList() {
@@ -13,17 +20,20 @@ function AsteroidsList() {
 
     const [ page, setPage] = useState(1)
 
+    const [date, setDate] = useState(new Date());
+
+    const convertedDate = date.toISOString().slice(0, 10)
 
     useEffect(() => {
       axios
               .get(
-                  `https://www.neowsapp.com/rest/v1/feed/today?api_key=3O93YcjVXfreFogJawULO4fHalcOlw8GYPn9BVut`
+                  `https://www.neowsapp.com/rest/v1/feed?start_date=${convertedDate}&end_date=${convertedDate}?api_key=3O93YcjVXfreFogJawULO4fHalcOlw8GYPn9BVut`
               )
               .then((response) => {
                   const convertedAsteroidsData = convertAsteroidsData(response.data)
                   setAsteroidsData(convertedAsteroidsData);
               });
-    }, [])
+    }, [convertedDate])
 
     let distanceUnitButton
 
@@ -35,10 +45,42 @@ function AsteroidsList() {
 
     const asteroidsShown = 5 * page
     const fullAsteroidsDataShown = asteroidsShown >= asteroidsData.length
-  
+    
 
   return (
     <div className='asteroids-list'>
+       <div>
+          <DatePicker
+              showIcon
+              selected={date}
+              onChange={(newDate) => setDate(newDate)}
+              locale="en-GB"
+              dateFormat="dd MMMM yyyy"
+              icon={
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="1em"
+                  height="1em"
+                  viewBox="0 0 48 48"
+                >
+                  <mask id="ipSApplication0">
+                    <g fill="none" stroke="#fff" strokeLinejoin="round" strokeWidth="4">
+                      <path strokeLinecap="round" d="M40.04 22v20h-32V22"></path>
+                      <path
+                        fill="#fff"
+                        d="M5.842 13.777C4.312 17.737 7.263 22 11.51 22c3.314 0 6.019-2.686 6.019-6a6 6 0 0 0 6 6h1.018a6 6 0 0 0 6-6c0 3.314 2.706 6 6.02 6c4.248 0 7.201-4.265 5.67-8.228L39.234 6H8.845l-3.003 7.777Z"
+                      ></path>
+                    </g>
+                  </mask>
+                  <path
+                    fill="currentColor"
+                    d="M0 0h48v48H0z"
+                    mask="url(#ipSApplication0)"
+                  ></path>
+                </svg>
+              }
+          />
+      </div>
         {distanceUnitButton}
             {asteroidsData.slice(0, asteroidsShown).map((asteroid) => {
               return (
