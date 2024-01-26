74d616fe106ef7bb4e1136c96e1cdf15f1b9f70b
Ksusha <ksuksuksuha@gmail.com>
2024-01-26 13:45:58 +0100

    flatten project structure

--- src/AsteroidsList.jsx
+++ src/AsteroidsList.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import './AsteroidsList.css/'
+import './AsteroidsList.css'
 import { useEffect, useState } from 'react';
 import axios from "axios";
 import { AsteroidCard } from './AsteroidCard.jsx';
 import comet from './assets/comet.png';
 import cometGrey from './assets/comet-grey.png'
-import LoadingSpinner from './LoadingSpinner';
+import LoadingSpinner from './LoadingSpinner.jsx';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
@@ -13,7 +13,7 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import 'dayjs/locale/en-gb'
 import dayjs from "dayjs"
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { APPROACH_DATE_FORMAT } from './constants';
+import { APPROACH_DATE_FORMAT } from './constants.js';
 
 
 function AsteroidsList(props) {
